ff08b1eb435a0440fc79b50882dadbbf162299a7
alexmdv <alex@nfnd.me>
2021-01-25 00:44:04 +0300

    Create mailuser-addnew.sh

---
+++ mailuser-addnew.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pgadmpass="PASSWORD"
+
+echo "Mailbox user creation script"
+# Read Password
+echo -n Mailbox Username:
+read -r usrname
+echo -n Password: 
+read -s password1
+echo
+echo -n Repeat password:
+read -s password2
+echo
+mysalt=$(pwgen 16 1)
+if [[ "$password1" == "$password2" ]]; then
+hss=$(echo -n $password1 | argon2 $mysalt -id -e)
+echo "insert into users values ('nfnd.me', :'usrname', :'pass')" | PGPASSWORD=$pgadmpass psql -U "postgres" -d "mail_server" -h 127.0.0.1 -v pass="$hss" -v usrname="$usrname"
+echo "Mailbox user created."
+echo "Bye!"
+else
+echo "ERROR: Wrong password!"
+fi
