f59e0d5d85ad56ece50636e7ed32c81f3028adae
alexmdv <alex@nfnd.me>
2021-01-31 21:20:50 +0300

    Update mailuser-setpass.sh
    Added additional check for empty username, domain name, password.

--- mailuser-setpass.sh
+++ mailuser-setpass.sh
@@ -5,20 +5,35 @@ echo "Mailbox password update script."
 # Read Password
 echo -n "Mailbox Username:"
 read -r usrname
+if [ -z "$usrname" ]; then
+    echo "Username can not be empty!"
+    echo "Bye!"
+    exit
+fi
 echo -n "Mailbox domain (example.com):"
 read -r dmnname
+if [ -z "$dmnname" ]; then
+    echo "Domain name can not be empty!"
+    echo "Bye!"
+    exit
+fi
 echo -n "New password:" 
 read -s password1
 echo
 echo -n "Repeat new password:"
 read -s password2
 echo
-mysalt=$(pwgen 16 1)
 if [[ "$password1" == "$password2" ]]; then
-hss=$(echo -n $password1 | argon2 $mysalt -id -e)
-echo "UPDATE users SET password_hash=:'pass' WHERE local=:'usrname' and domain=:'dmnname'" | PGPASSWORD=$pgadmpass psql -U "postgres" -d "mail_server" -h 127.0.0.1 -v pass="$hss" -v usrname="$usrname" -v dmnname="$dmnname"
-echo "Mailbox password updated."
-echo "Bye!"
+  if [ -z "$password1" ]; then
+    echo "Password can not be empty!"
+    echo "Bye!"
+    exit
+  fi
+  mysalt=$(pwgen 16 1)
+  hss=$(echo -n $password1 | argon2 $mysalt -id -e)
+  echo "UPDATE users SET password_hash=:'pass' WHERE local=:'usrname' and domain=:'dmnname'" | PGPASSWORD=$pgadmpass psql -U "postgres" -d "mail_server" -h 127.0.0.1 -v pass="$hss" -v usrname="$usrname" -v dmnname="$dmnname"
+  echo "Mailbox password updated."
+  echo "Bye!"
 else
-echo "ERROR: Wrong password!"
+  echo "ERROR: Wrong password!"
 fi
