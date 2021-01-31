04fadbcf4325debc63cdd500c79ec1e04166f6a0
alexmdv <alex@nfnd.me>
2021-01-31 20:19:36 +0300

    Update mailuser-addnew.sh

--- mailuser-addnew.sh
+++ mailuser-addnew.sh
@@ -5,6 +5,8 @@ echo "Mailbox user creation script"
 # Read Password
 echo -n Mailbox Username:
 read -r usrname
+echo -n Mailbox Domain:
+read -r dmnname
 echo -n Password: 
 read -s password1
 echo
@@ -14,7 +16,7 @@ echo
 mysalt=$(pwgen 16 1)
 if [[ "$password1" == "$password2" ]]; then
 hss=$(echo -n $password1 | argon2 $mysalt -id -e)
-echo "insert into users values ('nfnd.me', :'usrname', :'pass')" | PGPASSWORD=$pgadmpass psql -U "postgres" -d "mail_server" -h 127.0.0.1 -v pass="$hss" -v usrname="$usrname"
+echo "insert into users values (:'dmnname', :'usrname', :'pass')" | PGPASSWORD=$pgadmpass psql -U "postgres" -d "mail_server" -h 127.0.0.1 -v pass="$hss" -v dmnname="$dmnname" -v usrname="$usrname"
 echo "Mailbox user created."
 echo "Bye!"
 else
